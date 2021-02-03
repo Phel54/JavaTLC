0244309c2ae0fdf01fdb5f83fe0da10165b44a10
Isichei Phelim <isiphelim@gmail.com>
2021-02-03 21:22:28 +0000

    Adding question5

---
+++ src/Question5.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Question5 {
+    public static void main(String[] args) {
+    int i;
+    int j;
+        for(i = 1; i <= 12; ++i){
+            for (j=1; j<13; ++j){
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+    }
+}
